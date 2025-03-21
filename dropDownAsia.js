d1b30f1e60391b8cb50eef21344e27febd7bf034
julius-altmann-3333 <167089364+julius-altmann-3333@users.noreply.github.com>
2025-03-21 10:01:15 +0100

    Add files via upload

---
+++ dropDownAsia.js
@@ -0,0 +1,4 @@
+document.getElementById("toggleDropdownAsia").addEventListener("click", function() {
+    var dropdown_asia = document.getElementById("dropdownContentAsia");
+    dropdown_asia.style.display = (dropdown_asia.style.display === "none" || dropdown_asia.style.display === "") ? "block" : "none";
+});
